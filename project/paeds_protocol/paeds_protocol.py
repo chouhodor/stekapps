a0a77b4259e190a6eb853b07e85e19a8eb474606
chou <choukatanagari@yahoo.com>
2022-01-24 16:26:36 +0800

    update

--- project/paeds_protocol/paeds_protocol.py
+++ project/paeds_protocol/paeds_protocol.py
@@ -6,3 +6,8 @@ paeds_protocol_bp = Blueprint('paeds_protocol', __name__, url_prefix='/paeds_pro
 def index():
 
     return render_template('index.html')
+
+@paeds_protocol_bp.route('/ios')
+def ios():
+
+    return render_template('index_ios.html')
