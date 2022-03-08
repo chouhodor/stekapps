5a2992800a46dc6079d1ae94809e403c63f5e833
chouhodor <choukatanagari@yahoo.com>
2022-03-08 13:23:38 +0800

    update

--- project/snake_data/snake_data.py
+++ project/snake_data/snake_data.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template , url_for
+from flask import Blueprint, render_template , url_for, request, redirect
 from ..models import Snakes, Snake_location
 
 
@@ -6,13 +6,44 @@ snake_data_bp = Blueprint('snake_data', __name__, url_prefix='/snake-data', temp
 
 @snake_data_bp.route('/')
 def index():
+
+    snake_list = []
+
     return render_template('snake_index.html')
+
+@snake_data_bp.route('/get_snake_list', methods=['POST'])
+def get_snake_list():
+
+    region = request.form['region']
+
+    snake_list = Snake_location.query.filter_by(location=region).all()
+
+    return redirect(url_for('index'),
+    snake_list=snake_list
+    )
+
 
 @snake_data_bp.route('/test')
 def test():
 
-    snake_list = Snake_location.query.filter_by(snake_id=3).all()
+    #Job.query.filter(Job.id.notin_([j.id for j in person.jobs]))
 
-    print(snake_list)
+    snake_list = Snake_location.query.filter_by(state='pahang').all()
+
+    #snake_data = Snake_location.query.filter(Snake_location.id.in_(snake_id.locations))
+
+    data_list = []
+
+    for s in snake_list:
+        data_list.append(s.snake_id)
+
+    snake_data = [Snakes.query.get(i) for i in data_list]
+
+    snake_names = [a.name_en for a in snake_data]
+
+    print(snake_names)
+
+
+
 
     return render_template('snake_index.html')
